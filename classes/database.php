dd278a207fb977d098f5d5d72d49f5aec5abbb73
Your Name <you@example.com>
2025-05-08 10:49:43 +0800

    firct commit

---
+++ classes/database.php
@@ -0,0 +1,37 @@
+<?php
+ 
+class database{
+ 
+    function opencon(){
+        return new PDO(
+            'mysql:host=localhost; 
+            dbname=dbs_app',
+            username: 'root',
+            password: '');
+        }
+
+        function signupUser($firstname, $lastname, $username, $password){
+            $con = $this->opencon();
+
+            try{
+                $con->beginTransaction();
+
+                $stmt =$con->prepare("INSERT INTO Admin (admin_FN, 
+                admin_LN, admin_username, admin_password)VALUES(?,?,?,?)");
+                $stmt->execute([$firstname,$lastname, $username, $password]);
+
+                $userID = $con->lastInsertID();
+                $con->commit();
+
+                return $userID;
+            }catch (PDOExecption $e){
+                $con->rollBack();
+                return false;
+            }
+    
+
+        }
+
+
+    }
+
